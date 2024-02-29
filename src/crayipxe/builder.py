53eb7051c16f2021a8c83941eb27716b14a263d1
Mitch Harding (the weird one) <mitchell.harding@hpe.com>
2024-02-29 13:22:29 -0500

    Revert "Unparse bss script rawfile"
    This reverts commit 4d677818cdbf403a71b344c887554def4c4651fd.

--- src/crayipxe/builder.py
+++ src/crayipxe/builder.py
@@ -257,8 +257,8 @@ class BinaryBuilder(object):
         if self._bss_script_path:
             with open(self._bss_script_path, 'r') as bss_script_file:
                 local_bss_script = bss_script_file.read()
-        upstream_bss_script = api_instance.read_namespaced_config_map(self.configmap_name,
-                                                                      self.namespace).data.get('bss.ipxe')
+        upstream_bss_script = safe_load(api_instance.read_namespaced_config_map(self.configmap_name,
+                                                                      self.namespace).data.get('bss.ipxe'))
         if local_bss_script != upstream_bss_script:
             if not self._bss_script_path:
                 LOGGER.info("New BSS script available; first time build is flagged.")
