986a4500dc5d7465d06566786eb3c907434c2177
Russell Bunch <doomslayer@hpe.com>
2024-09-05 15:30:29 -0500

    Add rbg flag

--- src/crayipxe/builder.py
+++ src/crayipxe/builder.py
@@ -70,7 +70,7 @@ class BinaryBuilder(object):
     TIME_BETWEEN_BUILDCHECKS = 60
 
     def __init__(self):
-        self._build_options = ['httpcore:7', 'x509:7', 'efi_time:7', 'http:7', 'entropy:7', 'tls:7', 'drbg:7']
+        self._build_options = ['httpcore:7', 'x509:7', 'efi_time:7', 'http:7', 'entropy:7', 'tls:7', 'drbg:7', 'rbg:7']
         self._build_debug_options = ['httpcore:2', 'x509:2', 'efi_time']
 
         # Options and structures unique to the global configmap
